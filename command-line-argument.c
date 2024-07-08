78fd6c3ed12056ab8d59807354ad130e84346d99
Pranjal Sapkota <pranjalsapkota11@gmail.com>
2024-07-08 19:02:41 +0545

    logical change

--- command-line-argument.c
+++ command-line-argument.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 #include "cs50.h"
-int main( int argc ,string argv[])
+int main( int argc , string argv[])
 {
-  for (int  i = 1; i <= argc; i++)
+  for (int  i = 1; i < argc; i++)
   {
     printf("%s\t",argv[i]);
   }
-  /* here we make our program more efficent because initially we donot know how many word user type so whatever the user
-  type we return the same thing to the user in this program.
-  Note:if we print invalid argv location then it return us null value.
-  */
+  printf("%d",argc);
+  return 0;
+  /* Point to be noted:argc returns the total no of argument vector.it means it also includes the argv of location 0 so
+  total no becomes 1 more than the actual string that human type in the prompt.*/
 }
