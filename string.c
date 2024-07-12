26587f8555bb6404ab7008576c28f656db86c2d1
Pranjal Sapkota <pranjalsapkota11@gmail.com>
2024-07-12 22:30:39 +0545

    comparing string

--- string.c
+++ string.c
@@ -6,20 +6,25 @@ int main(void)
   char *s,*t;
    s=get_string("s:\n");
    t=get_string("t\n");
-     if (s==t)
+     /* if (s==t)
      {
         printf("same");
     }
     else{
         printf("different");
-    }
+    } */
+   /* above comparison is wrong because string is pointer and it points the zeroth location of string .so variables s and
+   t both points to the different location of memory where there contents are stored .or we can say they store the 
+   address of zeroth location of the string .*/
+    //correct way
+if (strcpy(s,t)==0)
+{
+    printf("same");
 }
-    //  string s=get_string("s:");
-// string t=get_string("t:");
-// if (s == t)
-// {
-    // printf("same");
-// }
-// else 
-// printf("different");
-// }
+else
+{
+    printf("different");
+}
+}
+
+    
